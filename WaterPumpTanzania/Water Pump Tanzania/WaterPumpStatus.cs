7c5b574eb0f86ee82257aa2a8f28c5d6218e093d
Casper <casper.vissers@rws.nl>
2023-10-19 08:24:21 +0200

    Water pump status class

---
+++ WaterPumpTanzania/Water Pump Tanzania/WaterPumpStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Water_Pump_Tanzania
+{
+    internal class WaterPumpStatus
+    {
+        public int Id { get; set; }
+        public string? StatusGroup { get; set; }
+    }
+}
