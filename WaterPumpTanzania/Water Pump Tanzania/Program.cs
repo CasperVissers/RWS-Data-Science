398b47fbc96680d74f1e64ccd8d8c384a7c5c37a
Casper <casper.vissers@rws.nl>
2023-10-19 08:10:59 +0200

    Visual Studio Setup

---
+++ WaterPumpTanzania/Water Pump Tanzania/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Water_Pump_Tanzania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
