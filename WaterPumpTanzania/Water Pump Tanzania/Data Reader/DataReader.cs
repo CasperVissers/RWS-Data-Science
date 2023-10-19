5ca4945ee143743dbf6bc85368a313325c1cb175
Casper <casper.vissers@rws.nl>
2023-10-19 08:46:12 +0200

    csv reader

---
+++ WaterPumpTanzania/Water Pump Tanzania/Data Reader/DataReader.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace Water_Pump_Tanzania.Data_Reader
+{
+    public static class DataReader
+    {
+        public static IEnumerable<T> ReadCsvData<T>(string path) where T : class
+        {
+            using var reader = new StreamReader(path);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            return csv.GetRecords<T>();
+        }
+    }
+}
